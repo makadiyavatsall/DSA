7d61f4b6477731ccb2b6f444c356a1d238b052da
Vatsal Makadiya <165473834+makadiyavatsall@users.noreply.github.com>
2025-08-26 01:27:44 +0530

    Time: 0 ms (100%), Space: 12.5 MB (52.41%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,8 +1,14 @@
 class Solution(object):
     def sortColors(self, nums):
-        n = len(nums)
-        for _ in range(n):  # multiple passes
-            for k in range(n - 1):
-                j = k + 1
-                if nums[k] > nums[j]:  # simple swap if out of order
-                    nums[k], nums[j] = nums[j], nums[k]
+        low, mid, high = 0, 0, len(nums) - 1
+        
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:  # nums[mid] == 2
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
