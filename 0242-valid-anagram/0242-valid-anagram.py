0d733aa680877a13b68e51d810659278c3a3ad9b
Vatsal Makadiya <165473834+makadiyavatsall@users.noreply.github.com>
2025-09-06 01:22:18 +0530

    Time: 27 ms (24.73%), Space: 13.7 MB (23.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,19 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if len(s) != len(t):   
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
+        s = list(s)
+        t = list(t)
+        if len(s) != len(t):
             return False
         
-        countS, countT = {}, {}  
-        
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)  
-            countT[t[i]] = 1 + countT.get(t[i], 0)  
-        
-        return countS == countT
+        for letter in s:
+            if letter in t:
+                t.remove(letter)
+            else:
+                return False
+        return True
