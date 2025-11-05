294f9ab3849d26a3f1ae86e476289de800c0b419
Vatsal Makadiya <165473834+makadiyavatsall@users.noreply.github.com>
2025-11-05 15:41:09 +0530

    Time: 27 ms (28.13%), Space: 19.3 MB (38.92%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,20 +1,25 @@
-class Solution(object):
-    def trap(self, height):
+class Solution:
+    def trap(self, height: List[int]) -> int:
         n = len(height)
-        max_left = [0] * n
-        max_right = [0] * n
+        if n == 0:
+            return 0
         
-        max_left[0] = height[0]
+        left_max = [0] * n
+        right_max = [0] * n
+        
+        # Fill left_max
+        left_max[0] = height[0]
         for i in range(1, n):
-            max_left[i] = max(max_left[i-1], height[i])
-            
-        max_right[n-1] = height[n-1]
-        for i in range(n-2, -1, -1):
-            max_right[i] = max(max_right[i+1], height[i])
-
-        total_water = 0
+            left_max[i] = max(left_max[i - 1], height[i])
+        
+        # Fill right_max
+        right_max[n - 1] = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            right_max[i] = max(right_max[i + 1], height[i])
+        
+        # Calculate trapped water
+        trapped = 0
         for i in range(n):
-            trapped_water_at_i = min(max_left[i], max_right[i]) - height[i]
-            total_water += trapped_water_at_i
-            
-        return total_water
+            trapped += min(left_max[i], right_max[i]) - height[i]
+        
+        return trapped
