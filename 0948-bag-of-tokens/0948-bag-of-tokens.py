fb35bfeff849b4902cb5d49b33860c6f61471712
Vatsal Makadiya <165473834+makadiyavatsall@users.noreply.github.com>
2026-02-19 14:41:39 +0530

    Time: 0 ms (100%), Space: 19.6 MB (11.32%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -0,0 +1,27 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        tokens.sort()
+        
+        l, r = 0, len(tokens) - 1
+        score = 0
+        max_score = 0
+        
+        while l <= r:
+            # Play smallest token face up if possible
+            if l <= r and power >= tokens[l]:
+                power -= tokens[l]
+                score += 1
+                l += 1
+                max_score = max(max_score, score)
+            
+            # Otherwise, play largest token face down if we have score to spend
+            elif l <= r and score > 0:
+                power += tokens[r]
+                score -= 1
+                r -= 1
+            
+            # No valid move left
+            else:
+                break
+        
+        return max_score
