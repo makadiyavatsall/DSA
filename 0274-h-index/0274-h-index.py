a50e3c2a9606e096322eaa6e8c7f3d80bd38d046
Vatsal Makadiya <165473834+makadiyavatsall@users.noreply.github.com>
2026-02-23 23:52:44 +0530

    Time: 0 ms (100%), Space: 19.6 MB (21.66%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,11 +1,11 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        # Step 1: Sort citations in descending order
+        
         citations.sort(reverse=True)
 
         h = 0
 
-        # Step 2: Check how many papers have at least (i+1) citations
+        
         for i in range(len(citations)):
             if citations[i] >= i + 1:
                 h = i + 1
