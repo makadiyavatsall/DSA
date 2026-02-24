31fdc9027bf836eaacba08457ee24aa4f8cc2017
Vatsal Makadiya <165473834+makadiyavatsall@users.noreply.github.com>
2026-02-24 23:49:39 +0530

    Time: 129 ms (37.79%), Space: 57.3 MB (8.58%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -0,0 +1,32 @@
+import random
+
+class RandomizedSet:
+    def __init__(self):
+        self.arr = []
+        self.pos = {}
+
+    def insert(self, val: int) -> bool:
+        if val in self.pos:
+            return False
+        self.pos[val] = len(self.arr)
+        self.arr.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.pos:
+            return False
+
+        idx = self.pos[val]
+        last_val = self.arr[-1]
+
+        # move last element to idx
+        self.arr[idx] = last_val
+        self.pos[last_val] = idx
+
+        # remove last
+        self.arr.pop()
+        del self.pos[val]
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.arr)
