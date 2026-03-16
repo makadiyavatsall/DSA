439a3b35a8ad09858a4a524ff9f3c6eaf9b77643
Vatsal Makadiya <165473834+makadiyavatsall@users.noreply.github.com>
2026-03-16 20:43:27 +0530

    Time: 19 ms (77.19%), Space: 25.6 MB (64.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,11 +3,11 @@ class Solution:
         n = len(nums)
         ans = [1] * n
 
-        # prefix (left products)
+        
         for i in range(1, n):
             ans[i] = ans[i-1] * nums[i-1]
 
-        # suffix (right products)
+        
         right = 1
         for i in range(n-1, -1, -1):
             ans[i] *= right
